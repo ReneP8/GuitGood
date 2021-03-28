7186e033102b6ec7557a485bbc37c9b52bd497f9
ReneP8 <rene-baving@web.de>
2021-03-28 15:16:55 +0200

    added buttons to the home screen and implemented boilerplate of the screens

---
+++ guit_good/lib/screens/create.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Create extends StatefulWidget {
+  @override
+  _CreateState createState() => _CreateState();
+}
+
+class _CreateState extends State<Create> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Center(child: Text('Add Song')));
+  }
+}
--- guit_good/lib/screens/home.dart
+++ guit_good/lib/screens/home.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:guit_good/screens/create.dart';
+import 'package:guit_good/screens/game.dart';
 
 class Home extends StatefulWidget {
   final String title;
@@ -14,8 +16,33 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text(widget.title)),
-      body: Center(child: Text('Home' , style: TextStyle(fontSize: 120.0),)),
-    );
+        appBar: AppBar(title: Text(widget.title)),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              TextButton.icon(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => Game()),
+                    );
+                  },
+                  icon: Icon(Icons.play_arrow),
+                  label: Text('Play Game')),
+              SizedBox(height: 20.0),
+              TextButton.icon(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => Create()),
+                    );
+                  },
+                  icon: Icon(Icons.add_box),
+                  label: Text('Add Song')),
+            ],
+          ),
+        ));
   }
 }
